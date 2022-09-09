efaee477b4b5bcf0968fe3c34fcf198fe020ad10
FY Kuo <teresa99339@gmail.com>
2022-09-09 09:19:59 +0800

    整理檔案 預先新增需要之檔案 調整css 建立共用css 跑馬燈news畫面完成

---
+++ front/news.php
@@ -0,0 +1,19 @@
+<h1 class="ct">最新消息</h1>
+
+<table class="w-80 aut">
+    <tr class="tt">
+        <th>
+            標題
+        </th>
+    </tr>
+    <tr class="pp">
+        <td>
+        情人節特惠活動
+        </td>
+    </tr>
+    <tr class="pp">
+        <td>
+        年終特賣會開跑了
+        </td>
+    </tr>
+</table>
